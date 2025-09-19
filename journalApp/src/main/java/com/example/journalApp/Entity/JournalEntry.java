8fa35d8e3809c7690afdf2eae0153a8254b46606
aryanupadhyay29 <aryanupadhyay9027@gmail.com>
2025-09-19 18:09:30 +0530

    Add User in the entity

--- journalApp/src/main/java/com/example/journalApp/Entity/JournalEntry.java
+++ journalApp/src/main/java/com/example/journalApp/Entity/JournalEntry.java
@@ -1,38 +1,20 @@
 package com.example.journalApp.Entity;
 
 import com.example.journalApp.Controller.JournalEntryController;
+import jdk.jfr.DataAmount;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+
+@Data
+@NoArgsConstructor
 public class JournalEntry  {
-    private long id;
+
+    private ObjectId id;
     private String title;
     private String content;
+    private LocalDateTime date;
 
-    public JournalEntry() {
     }
-
-
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-}
---
+++ journalApp/src/main/java/com/example/journalApp/Entity/User.java
@@ -0,0 +1,15 @@
+package com.example.journalApp.Entity;
+
+
+import lombok.Data;
+import org.springframework.stereotype.Indexed;
+
+@Data
+public class User {
+
+    private Long id;
+
+    private String userName;
+
+    private String password;
+}
